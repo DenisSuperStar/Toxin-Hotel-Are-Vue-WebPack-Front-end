1f3889321afd60df0516d1a1b778971d66864182
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-04-01 20:26:47 +0300

    created new component and add the file variables

---
+++ app/components/PaginationText/PaginationText.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    div(class="pagination-text") 1 - 12 из 100+ вариантов аренды
+</template>
+
+<style lang="scss" scoped>
+    @import '../../mixinApp.scss';
+    @import '../PaginationText/mixinPaginationText.scss';
+
+    .pagination-text {
+        display: flex;
+        justify-content: center;
+        padding: 0;
+        font-size: $pagination-text_size;
+        line-height: $pagination-text_height;
+        color: $pagination-text_color;
+        font-family: $app_font;
+    }
+
+    @media screen and (min-width: 1200px) and (max-width: 1450px) {
+        .pagination-text {
+            padding: 0 0 0 48.26px;
+        }
+    }
+
+    @media screen and (min-width: 1451px) and (max-width: 1600px) {
+        .pagination-text {
+            display: flex;
+            justify-content: flex-end;
+        }
+    }
+</style>
+
+<script>
+    export default {
+      name: 'pagination_text'  
+    }
+</script>
