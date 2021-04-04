91dc902db94ca6710a1a95ebce89279823471364
Denis Shklyarskij <den.shklyarskij@bk.ru>
2021-04-04 15:40:34 +0300

    add mixin templates

--- app/components/RangeSlider/RangeSlider.vue
+++ app/components/RangeSlider/RangeSlider.vue
@@ -1,54 +1,38 @@
 <template lang="pug">
-    div()
-        div(style="display: flex; justify-content: space-between;")
-            div(class="title-range") диапазон
-            div(class="title-range-price") 5 000<span class="title-range-price__rub"></span> - 10 000<span class="title-range-price__rub"></span>
-        div(class="sidebar-box__range-slider")
-            div(class="range-slider" @click="apperField")
+    div(class="range-slider")
+        div(class="range-slider__label-box")
+            div(class="range-slider__range-label") диапазон
+            div(class="range-slider__price") {{begin_range}}
+                span(class="range-slider__rub") - {{end_range}}
+                span(class="range-slider__rub")
+        div(class="range-slider__slider-box")
+            div(
+                class="range-slider__slider" 
+                @click="appearDisplayField"
+            )
                 input(
-                    type="range" 
+                    type="range"
                     class=`
-                        range-slider__range 
-                        current-color
-                        range-slider__range--colored` 
-                    min="5000" 
-                    max="10000" 
-                    step="100" 
+                        range-slider__range
+                        range-slider__range--first
+                        range-slider__range--current-colored
+                    `
+                    min="0"
+                    max="10000"
+                    step="100"
                     v-model="sliderMin"
                 )
                 input(
-                    type="number" 
-                    class=`
-                        range-slider__number 
-                        range-slider__number--colored 
-                        range-slider__number--transparented` 
-                    min="5000" 
-                    max="10000" 
-                    step="100" 
-                    v-model="sliderMin" 
-                    v-show="show_field"
-                )
-                input(
-                    type="range" 
+                    type="range"
                     class=`
                         range-slider__range
-                        current-color 
-                        range-slider__range--colored` 
-                    min="5000" 
-                    max="10000" 
-                    step="100"  
+                        range-slider__range--last
+                        range-slider__range--current-colored
+                    `
+                    min="10000"
+                    max="20000"
+                    step="100"
                     v-model="sliderMax"
-                )
-                input(
-                    type="number" 
-                    class=`
-                        range-slider__number 
-                        range-slider__number--colored 
-                        range-slider__number--transparented` 
-                    min="5000" 
-                    max="10000" 
-                    step="100" 
-                    v-model="sliderMax" v-show="show_field"
                 )
 </template>
 
@@ -57,41 +41,34 @@
         name: 'range-slider',
         data: () => {
             return {
-                begin_range: 6500,
-                end_range: 8500,
+                begin_range: 6300,
+                end_range: 13600,
                 show_field: false
             }
         },
         methods: {
-           apperField: function() {
-               this.show_field =!this.show_field;
+           appearDisplayField: function() {
+               this.display_field =!this.display_field;
            }
         },
         computed: {
             sliderMin: {
                 get: function() {
-                    const number = +(this.begin_range);
-                    return number;
+                    return +(this.begin_range);
                 },
-                set: function(number) {
-                    const value = +(number);
-                    if (value > this.end_range) {
-                        this.end_range = value;
-                    }
-                    this.begin_range = value;
+                set: function(value) {
+                    this.begin_range = +(value);
                 }
             },
             sliderMax: {
                 get: function() {
-                    const number = +(this.end_range);
-                    return number;
+                    return +(this.end_range);
                 },
-                set: function(number) {
-                    const value = +(number);
-                    if (value < this.begin_range) {
-                        this.begin_range = value;
-                    }
+                set: function(value) {
                     this.end_range = value;
+                    if (this.end_range < 10000) {
+                        this.end_range = 10000;
+                    }
                 }
             }
         }
@@ -99,103 +76,60 @@
 </script>
 
 <style lang="scss" scoped>
-
+    @import '../../mixinApp.scss';
     @import '../RangeSlider/mixinRangeSlider.scss';
 
-    .range-colored {
-        background: $control_background;
+    @mixin range(
+        $range-self_width,
+        $range-self_height,
+        $range-self_background,
+        $range-self_default-border-thick,
+        $range-self_default_border-style,
+        $range-self_default_border-color,
+        $range-self_round
+    ) {
+        width:          $range-self_width;
+        height:         $range-self_height;
+        background:     $range-self_background;
+        border-width:   $range-self_default-border-thick;
+        border-style:   $range-self_default_border-style;
+        border-color:   $range-self_default_border-color;
+        border-radius:  $range-self_round
     }
 
-    .range-slider {
-        position: relative;
-        width: 100%; /*На расширении где-то в 1420px указать точные размеры всех элементов по макету*/
-        text-align: center;
-        height: max-content;
-        padding: 23px 0 0 0;
-        cursor: pointer;
-        &__range {
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            appearance: none;
-            width: 100%;
-            height: $range-slider_height;
-            border-radius: $range-slider_rounded_corner;
-            background: $range-slider_default_background;
-            border: $range-slider_default_border;
-        }
-        &__range::-webkit-slider-thumb {
-            z-index: 2;
-            position: relative;
-            appearance: none;
-            width: $control_width;
-            height: $control_height;
-
-            border-width: $control_default_border;
-            border-style: $control_default_border;
-            border-color: $control_default_border;
-
-            border-radius: $control_rounding;
-            background: $control_default_background;
-            cursor: pointer;
-        }
-        &__range {
-            /*
-                range-slider style
-            */
-            &--colored.current-color {
-                @extend .range-colored;
-            }
-            /*
-                control of range-slider styles
-            */
-            &--colored.current-color::-webkit-slider-thumb {
-                @extend .range-colored;
-            }
-        }
-        &__number {
-            width: $field_width;
-            text-align: center;
-            color: $field_default_color;
-            background: $field_default_background;
-            border-width: $field_default_border-thick;
-            border-style: $field_default_border-style;
-            border-color: $field_default_border-color;
-            border-radius: $field_rounding;
-            margin: 0 0 23.44px 0;
-            font: 14px / 24px 'Montserrat', sans-serif;
-            &:last-child {
-                margin: 0 0 23.44px 103.44px;
-            }
-            &--colored {
-                border-width: $field_border-thick;
-                border-style: $field_border-style;
-                border-color: $field_border-color;
-                color: $field_color;
-                background: $field_background;
-            }
-            &--transparented {
-                opacity: $field_opacity;
-            }
-            &::-webkit-outer-spin-button,
-            &::-webkit-inner-spin-button {
-                appearance: none;
-            }
-            /*
-                If out of range then input fields view with color danger
-            */
-            &:invalid,
-            &:out-of-range {
-                /*border: 2px solid #ff6347;*/
-                border-width: $field_invalid_border-thick;
-                border-style: $field_invalid_border-style;
-                border-color: $field_invalid_border-color;
-            }
-        }
+    @mixin thumb(
+        $thumb_width,
+        $thumb_height,
+        $thumb_default_background,
+        $thumb_default_border-thick,
+        $thumb_default_border-style,
+        $thumb_default-border-color,
+        $thumb_round
+    ) {
+        width:          $thumb_width;
+        height:         $thumb_height;
+        background:     $thumb_default_background;
+        border-width:   $thumb_default_border-thick;
+        border-style:   $thumb_default_border-style;
+        border-color:   $thumb_default_border-color;
+        border-radius:  $thumb_round;
     }
+
+    @mixin rangeCurrent(
+        $range_background
+    ) {
+        background: $range_background;
+    }
+
+    @mixin thumbCurrent(
+        $thumb_background
+    ) {
+        background: $thumb_background;
+    }
+
 
     @media screen and (min-width: 1200px) and (max-width: 1440px) {
-        .range-slider {
+        .slider {
             display: flex;
             &__range::-webkit-slider-thumb {
                 width: $control_width_less;
@@ -204,28 +138,85 @@
         }
     }
 
-    .title-range {
-        font-weight: bold;
-        font-size: 12px;
-        line-height: 15px;
-        text-transform: uppercase;
-        color: #1f2041;
-        font-family: 'Montserrat', sans-serif;
-    }
-
-    .title-range-price {
-        font-size: 12px;
-        line-height: 15px;
-        text-transform: uppercase;
-        color: rgba(31, 32, 65, 0.5);
-        font-family: 'Montserrat', sans-serif;
-    }
-
-    .title-range-price__rub::after {
-        content: '\f158';
-        font-family: 'Font Awesome 5 Pro';
-        color: rgba(31, 32, 65, 0.5);
-        font-size: 11px;
-        line-height: 15px;
+    .range-slider {
+        &__label-box {
+            display: flex;
+            justify-content: space-between;
+        }
+        &__range-label,
+        &__price {
+            font-size: 12px;
+            line-height: 15px;
+            text-transform: uppercase;
+            font-family: $app_font;
+        }
+        &__range-label {
+            font-weight: bold;
+            color: #1f2041;
+        }
+        &__price {
+            &, &::after {color: rgba(31, 32, 65, 0.5);}
+            &::after {
+                content: '\f158';
+                font-size: 11px;
+                line-height: 15px;
+                font-family: $app_font;
+            }
+        }
+        &__slider {
+            position: relative;
+            width: 100%; 
+            /*???*/
+            /*На расширении где-то в 1420px указать точные размеры всех элементов по макету*/
+            text-align: center;
+            padding: 23px 0 0 0;
+            cursor: pointer;
+        }
+        &__range,
+        &__range::-webkit-slider-thumb {
+            appearance: none;
+        }
+        &__range {
+            position: absolute;
+            cursor: pointer;
+            &--first {
+                left: 0;
+            }
+            &--last {
+                right: 0;
+            }
+            @include range(
+                $range-slider_width,
+                $range-slider_height,
+                $range-slider_default_background,
+                $range-slider_default_border-width,
+                $range-slider_default_border-style,
+                $range-slider_default_border-color,
+                $range-slider_round
+            );
+            &::-webkit-slider-thumb {
+                @include thumb(
+                    $slider-thumb_width,
+                    $slider-thumb_height,
+                    $slider-thumb_default_background,
+                    $slider-thumb_default_border-width,
+                    $slider-thumb_default_border-style,
+                    $slider-thumb_default_border-color,
+                    $slider-thumb_round
+                );
+                cursor: pointer;
+                z-index: 2;
+            }
+            &--current-colored {
+                @include rangeCurrent(
+                    $range-slider_background
+                );
+            }
+            &--current-colored::-webkit-slider-thumb {
+                @include thumbCurrent(
+                    $slider-thumb_background
+                );
+            }
+        }
     }
 </style>
